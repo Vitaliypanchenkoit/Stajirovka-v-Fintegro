2d691ccd8d865603d9b93ed3ee12e8a771673389
Vitaliy Panchenko <Vitaliypanchenko.it@gmail.com>
2018-04-16 13:15:18 +0300

    Add 2 methods into 'f_count_letters.php'

--- f_cout_letters.php
+++ f_cout_letters.php
@@ -21,6 +21,16 @@ function count_letters ($string){
 }
 count_letters('nfnfnfwnf');
 
+//Method 1.1 Use function "array_count_values"
+function count_letters_1 ($string){
+    $array_of_letters = str_split($string);
+    $array_of_letters = array_count_values($array_of_letters);
+	arsort($array_of_letters);
+
+	print_r($array_of_letters);
+}
+count_letters_1('nfnfnfffwnf');
+
 //Method 2 - Optimal
 function count_letters_2($string){
     $array = [];
@@ -36,3 +46,17 @@ function count_letters_2($string){
 	print_r($array);
 }
 count_letters_2('nfnfnfwnf');
+
+//Method 2.2 Use function count_chars
+function count_letters_3($string){
+    $sorted_string = count_chars($string, 1);
+    $array = [];
+	foreach ($sorted_string as $letter => $val){
+        $letter = chr($letter);
+        $array[$letter] = $val;
+
+		//echo chr($letter) . "=".  $val . ";" . "\n";
+    }
+    var_dump($array);
+}
+count_letters_3('nfnfnfwnf');
